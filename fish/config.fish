a80ea07d472349987d48d43ffbb0b0a17400e0b9
Nathaniel Fishel <nathaniel.fishel@autodesk.com>
2024-02-06 11:59:17 -0500

    backup configs

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,8 @@ if status is-interactive
     source ~/.iterm2_shell_integration.fish
 end
 fish_add_path /usr/local/opt/mysql-client/bin
+alias vlime='sbcl --load ~/.local/share/nvim/site/pack/packer/start/vlime/lisp/start-vlime.lisp'
 fish_vi_key_bindings
+
+# opam configuration
+source /Users/fisheln/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
